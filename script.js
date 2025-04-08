4bba58606fe27a9e997355341dee7c5110641175
Ssssoar <jano20@vtr.net>
2025-04-08 14:07:05 -0400

    Basic setup

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-alert("TESTING");
+var power = 0;
+
+/*hecharle un solo carbon a la maquina*/
+function unCarbon(){
+    var meter = document.querySelector("#Power-Meter");
+    power++;
+    meter.innerHTML = power + " Caballos de Fuerza"
+}
+
+/*function to fill an already selected query from a file given as a string*/
+function fillFromFile(selected,fileURL) {
+    fetch(fileURL)
+    .then(response => response.text())
+    .then(textString => {
+        selected.innerHTML = textString;
+    });
+}
