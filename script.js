8f4dd218449db216d44759f717908c3176ec46cd
Ssssoar <jano20@vtr.net>
2025-04-08 17:00:27 -0400

    end of mid-day progress

--- script.js
+++ script.js
@@ -4,7 +4,51 @@ var firstPower = true;
 var initialLoadCost = 5
 var header1Cost = 20
 var header2Cost = 200
-var header3Cost = 2000
+var game1_1Cost = 2000
+var game1_1Cost = 50
+var game1_2Cost = 500
+var game1_3Cost = 5000
+var game1_4Cost = 5000
+var game2_1Cost = 80
+var game2_2Cost = 800
+var game2_3Cost = 8000
+var game2_4Cost = 8000
+var game3_1Cost = 110
+var game3_2Cost = 1100
+var game3_3Cost = 11000
+var game3_4Cost = 11000
+var game4_1Cost = 140
+var game4_2Cost = 1400
+var game4_3Cost = 14000
+var game4_4Cost = 14000
+var game5_1Cost = 170
+var game5_2Cost = 1700
+var game5_3Cost = 17000
+var game5_4Cost = 17000
+var game6_1Cost = 200
+var game6_2Cost = 2000
+var game6_3Cost = 20000
+var game6_4Cost = 20000
+var game7_1Cost = 230
+var game7_2Cost = 2300
+var game7_3Cost = 23000
+var game7_4Cost = 23000
+var game8_1Cost = 260
+var game8_2Cost = 2600
+var game8_3Cost = 26000
+var game8_4Cost = 26000
+var game9_1Cost = 290
+var game9_2Cost = 2900
+var game9_3Cost = 29000
+var game9_4Cost = 29000
+var game10_1Cost = 320
+var game10_2Cost = 3200
+var game10_3Cost = 32000
+var game10_4Cost = 32000
+var game11_1Cost =  350
+var game11_2Cost = 3500
+var game11_3Cost = 35000
+var game11_4Cost = 35000
 
 setInterval(step,0.5)
 
@@ -30,12 +74,100 @@ function meterUpdate(){
 }
 
 function updateCosts(){
-    var cost1 = document.querySelector("#header1cost")
-    if (cost1 != null){cost1.innerHTML = header1Cost;}
-    var cost2 = document.querySelector("#header2cost")
-    if (cost2 != null){cost2.innerHTML = header2Cost;}
-    var cost3 = document.querySelector("#header3cost")
-    if (cost3 != null){cost3.innerHTML = header3Cost;}
+    var cost = document.querySelector("#header1cost")
+    if (cost != null){cost.innerHTML = header1Cost;}
+    cost = document.querySelector("#header2cost")
+    if (cost != null){cost.innerHTML = header2Cost;}
+    var cost = document.querySelector("#header3cost")
+    if (cost != null){cost.innerHTML = header3Cost;}
+    var cost = document.querySelector("#game1_1Cost")
+    if (cost != null){cost.innerHTML = game1_1Cost;}
+    var cost = document.querySelector("#game1_2Cost")
+    if (cost != null){cost.innerHTML = game1_2Cost;}
+    var cost = document.querySelector("#game1_3Cost")
+    if (cost != null){cost.innerHTML = game1_3Cost;}
+    var cost = document.querySelector("#game1_4Cost")
+    if (cost != null){cost.innerHTML = game1_4Cost;}
+    var cost = document.querySelector("#game2_1Cost")
+    if (cost != null){cost.innerHTML = game2_1Cost;}
+    var cost = document.querySelector("#game2_2Cost")
+    if (cost != null){cost.innerHTML = game2_2Cost;}
+    var cost = document.querySelector("#game2_3Cost")
+    if (cost != null){cost.innerHTML = game2_2Cost;}
+    var cost = document.querySelector("#game2_4Cost")
+    if (cost != null){cost.innerHTML = game2_4Cost;}
+    var cost = document.querySelector("#game3_1Cost")
+    if (cost != null){cost.innerHTML = game3_1Cost;}
+    var cost = document.querySelector("#game3_2Cost")
+    if (cost != null){cost.innerHTML = game3_2Cost;}
+    var cost = document.querySelector("#game3_3Cost")
+    if (cost != null){cost.innerHTML = game3_2Cost;}
+    var cost = document.querySelector("#game3_4Cost")
+    if (cost != null){cost.innerHTML = game3_4Cost;}
+    var cost = document.querySelector("#game4_1Cost")
+    if (cost != null){cost.innerHTML = game4_1Cost;}
+    var cost = document.querySelector("#game4_2Cost")
+    if (cost != null){cost.innerHTML = game4_2Cost;}
+    var cost = document.querySelector("#game4_3Cost")
+    if (cost != null){cost.innerHTML = game4_2Cost;}
+    var cost = document.querySelector("#game4_4Cost")
+    if (cost != null){cost.innerHTML = game4_4Cost;}
+    var cost = document.querySelector("#game5_1Cost")
+    if (cost != null){cost.innerHTML = game5_1Cost;}
+    var cost = document.querySelector("#game5_2Cost")
+    if (cost != null){cost.innerHTML = game5_2Cost;}
+    var cost = document.querySelector("#game5_3Cost")
+    if (cost != null){cost.innerHTML = game5_2Cost;}
+    var cost = document.querySelector("#game5_4Cost")
+    if (cost != null){cost.innerHTML = game5_4Cost;}
+    var cost = document.querySelector("#game6_1Cost")
+    if (cost != null){cost.innerHTML = game6_1Cost;}
+    var cost = document.querySelector("#game6_2Cost")
+    if (cost != null){cost.innerHTML = game6_2Cost;}
+    var cost = document.querySelector("#game6_3Cost")
+    if (cost != null){cost.innerHTML = game6_2Cost;}
+    var cost = document.querySelector("#game6_4Cost")
+    if (cost != null){cost.innerHTML = game6_4Cost;}
+    var cost = document.querySelector("#game7_1Cost")
+    if (cost != null){cost.innerHTML = game7_1Cost;}
+    var cost = document.querySelector("#game7_2Cost")
+    if (cost != null){cost.innerHTML = game7_2Cost;}
+    var cost = document.querySelector("#game7_3Cost")
+    if (cost != null){cost.innerHTML = game7_2Cost;}
+    var cost = document.querySelector("#game7_4Cost")
+    if (cost != null){cost.innerHTML = game7_4Cost;}
+    var cost = document.querySelector("#game8_1Cost")
+    if (cost != null){cost.innerHTML = game8_1Cost;}
+    var cost = document.querySelector("#game8_2Cost")
+    if (cost != null){cost.innerHTML = game8_2Cost;}
+    var cost = document.querySelector("#game8_3Cost")
+    if (cost != null){cost.innerHTML = game8_2Cost;}
+    var cost = document.querySelector("#game8_4Cost")
+    if (cost != null){cost.innerHTML = game8_4Cost;}
+    var cost = document.querySelector("#game9_1Cost")
+    if (cost != null){cost.innerHTML = game9_1Cost;}
+    var cost = document.querySelector("#game9_2Cost")
+    if (cost != null){cost.innerHTML = game9_2Cost;}
+    var cost = document.querySelector("#game9_3Cost")
+    if (cost != null){cost.innerHTML = game9_2Cost;}
+    var cost = document.querySelector("#game9_4Cost")
+    if (cost != null){cost.innerHTML = game9_4Cost;}
+    var cost = document.querySelector("#game10_1Cost")
+    if (cost != null){cost.innerHTML = game10_1Cost;}
+    var cost = document.querySelector("#game10_2Cost")
+    if (cost != null){cost.innerHTML = game10_2Cost;}
+    var cost = document.querySelector("#game10_3Cost")
+    if (cost != null){cost.innerHTML = game10_2Cost;}
+    var cost = document.querySelector("#game10_4Cost")
+    if (cost != null){cost.innerHTML = game10_4Cost;}
+    var cost = document.querySelector("#game11_1Cost")
+    if (cost != null){cost.innerHTML = game11_1Cost;}
+    var cost = document.querySelector("#game11_2Cost")
+    if (cost != null){cost.innerHTML = game11_2Cost;}
+    var cost = document.querySelector("#game11_3Cost")
+    if (cost != null){cost.innerHTML = game11_2Cost;}
+    var cost = document.querySelector("#game11_4Cost")
+    if (cost != null){cost.innerHTML = game11_4Cost;}
 }
 
 function initialLoad(){
@@ -55,7 +187,7 @@ function loadHeader2(){
 
 function loadHeader3(){
     var header = document.querySelector("header");
-    spendToFill("power", header3Cost, header, "fillerhtmls/headers/header-3.html")
+    spendToFill("power", game1_1Cost, header, "fillerhtmls/headers/header-3.html")
 }
 
 function spendToFill(resource, cost, selected, fileURL){
